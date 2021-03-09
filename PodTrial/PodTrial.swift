dcbdd2eaa34de0c6c61c148ef2a04578b6a74511
KonoTatsuya <kono.tatsuya@moneyforward.co.jp>
2021-03-09 22:16:49 +0900

    add test cases

---
+++ PodTrial/PodTrial.swift
@@ -0,0 +1,20 @@
+//
+//  PodTrial.swift
+//  PodTrial
+//
+//  Created by kono.tatsuya on 2021/03/09.
+//
+
+public final class PodTrial {
+
+    let name = "PodTrial"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
