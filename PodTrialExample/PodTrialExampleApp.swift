5933582def8668f9c828231382835b25994253a6
KonoTatsuya <kono.tatsuya@moneyforward.co.jp>
2021-03-09 21:15:37 +0900

    first commit

---
+++ PodTrialExample/PodTrialExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodTrialExampleApp.swift
+//  PodTrialExample
+//
+//  Created by kono.tatsuya on 2021/03/09.
+//
+
+import SwiftUI
+
+@main
+struct PodTrialExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
